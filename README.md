be6db7cc71ec659ad0c97feee65ef8dd773d0c35
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-02-21 16:29:34 +0100

    Added working double pendulum

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Double Pendulum
+Project is based on this [website](https://www.myphysicslab.com/pendulum/double-pendulum-en.html)
--- src/com/company/DoublePendulum.java
+++ src/com/company/DoublePendulum.java
@@ -9,23 +9,17 @@ import java.awt.image.BufferedImage;
 
 public class DoublePendulum extends JPanel implements ActionListener {
 
-    Timer timer = new Timer(5,this);
+    Timer timer;
+    static double g = 1;
 
-    static int g = 10;
-
-    int radius1 = 200;
+    int radius1 = 100;
     int radius2 = 200;
     int mass1 = 20;
     int mass2 = 20;
-    double angle1 = (float)Math.PI/2;
-    double angle2 = (float)Math.PI/2;
-    int v_a1 = 0;
-    int v_a2 = 0;
-
-    int px2 = -1;
-    int py2 = -1;
-    int cx = this.getWidth() / 2;
-    int cy = 50;
+    double angle1 = Math.PI/2;
+    double angle2 = Math.PI/2;
+    double velocity_angle1 = 0;
+    double velocity_angle2 = 0;
 
     int x_1;
     int y_1;
@@ -38,7 +32,7 @@ public class DoublePendulum extends JPanel implements ActionListener {
     Point p1;
     Point p2;
     public DoublePendulum(){
-
+        timer = new Timer(1,this);
         bufferedImage = new BufferedImage(1000,1000,BufferedImage.TYPE_INT_RGB);
         g2d = (Graphics2D) bufferedImage.getGraphics();
         g2d.setColor(getBackground());
@@ -62,26 +56,44 @@ public class DoublePendulum extends JPanel implements ActionListener {
 
         g2d.setColor(Color.black);
         if(line2d!=null)
-        g2d.draw(line2d);
+            g2d.draw(line2d);
 
         timer.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        double O1_up = -g * (2*mass1 + mass2) * Math.sin(angle1)
+                - mass2 * g * Math.sin(angle1 - 2 * angle2)
+                - 2 * Math.sin(angle1-angle2) * mass2
+                * ((velocity_angle2 * velocity_angle2 * radius2) + (velocity_angle1 * velocity_angle1 * radius1 * Math.cos(angle1 - angle2)));
+        double O1_down = radius1 * (2*mass1 + mass2 - mass2 * Math.cos(2*angle1- 2*angle2));
+        double O1 = O1_up/O1_down;
+
+        double O2_up = 2*Math.sin(angle1 - angle2) * (velocity_angle1 * velocity_angle1 * radius1 * (mass1 + mass2)
+                + g * (mass1 + mass2) * Math.cos(angle1)
+                + velocity_angle2 * velocity_angle2 * radius2 * mass2 * Math.cos(angle1 - angle2));
+        double O2_down = radius2 * (2*mass1 + mass2 - mass2 * Math.cos(2*angle1- 2*angle2));
+        double O2 = O2_up/O2_down;
+
+        velocity_angle1 += O1;
+        velocity_angle2 += O2;
+        angle1 += velocity_angle1;
+        angle2 += velocity_angle2;
+
         double new_x1 = radius1 * Math.sin(angle1);
         double new_y1 = radius1 * Math.cos(angle1);
 
         double new_x2 = new_x1 + radius2 * Math.sin(angle2);
         double new_y2 = new_y1 + radius2 * Math.cos(angle2);
 
-        x_2 = (int)new_x2;
-        y_2 = (int)new_y2;
         x_1 = (int)new_x1;
         y_1 = (int)new_y1;
+        x_2 = (int)new_x2;
+        y_2 = (int)new_y2;
 
-        angle1 += .01;
-        angle2 += .02;
+        //velocity_angle1 *= .999;
+        //velocity_angle2 *= .999;
 
         p1 = new Point(x_2 + 400,y_2 + 400);
         if(p2!=null)
